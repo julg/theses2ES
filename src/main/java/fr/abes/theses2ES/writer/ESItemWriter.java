73af81fe80de90de1315d9ec4a359d48e7c590f7
Julien Gibert <gibert@abes.fr>
2019-11-03 21:01:36 +0100

    [Refactor] using bulk API to send data to ES

--- src/main/java/fr/abes/theses2ES/writer/ESItemWriter.java
+++ src/main/java/fr/abes/theses2ES/writer/ESItemWriter.java
@@ -1,29 +1,37 @@
 package fr.abes.theses2ES.writer;
 
-import java.util.List;
-
+import fr.abes.theses2ES.dto.TheseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.batch.item.ItemWriter;
 
-import fr.abes.theses2ES.dto.TheseDTO;
+import java.util.List;
 
+@Slf4j
 public class ESItemWriter implements ItemWriter<TheseDTO> {
-
-    private RequestOptions requestOptions = RequestOptions.DEFAULT;
 
     @Override
     public void write(List<? extends TheseDTO> items) throws Exception {
 
+        BulkRequest bulkRequest = new BulkRequest();
+        RequestOptions requestOptions = RequestOptions.DEFAULT;
+
         for (TheseDTO theseDTO : items) {
             IndexRequest request = new IndexRequest();
-            request.index("theses");
             request.id(String.valueOf(theseDTO.getIdDoc()));
+            request.index("theses");
             String jsonString = theseDTO.getJson();
             request.source(jsonString, XContentType.JSON);
-            ElasticClient.getClient().index(request, requestOptions);
+            bulkRequest.add(request);
+        }
+        BulkResponse bulkResponse = ElasticClient.getClient().bulk(bulkRequest, requestOptions);
+        if (bulkResponse.hasFailures()) {
+            int nbErrors = bulkResponse.getItems().length;
+            log.error(nbErrors + " errors.");
         }
     }
-
 }
